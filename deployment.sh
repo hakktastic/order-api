4c916b650b38041784bdfdabb9f9c59b332ea909
hakktastic <hakktastic@gmail.com>
2023-02-25 15:32:37 +0100

    HAK-107 remove build of container images
    - to separate build from deploy
    - see build.sh

--- deployment.sh
+++ deployment.sh
@@ -1,38 +1,6 @@
 echo "------------------------------------------------------------------------"
-echo " Build & Deploy Order API"
+echo " Deploy Order API"
 echo "------------------------------------------------------------------------"
-
-echo "------------------------------------------------------------------------"
-echo " Building Container images"
-echo "------------------------------------------------------------------------"
-
-echo "----------------------- Building Cart Service --------------------------"
-cd cart
-mvn clean package -DskipTests
-docker build -t hakktastic/demo-cart-service:0.0.1 .
-docker push hakktastic/demo-cart-service:0.0.1
-cd ..
-
-echo "----------------------- Building Order Service -------------------------"
-cd order
-mvn clean package -DskipTests
-docker build -t hakktastic/demo-order-service:0.0.1 .
-docker push hakktastic/demo-order-service:0.0.1
-cd ..
-
-echo "----------------------- Building Product Service -----------------------"
-cd product
-mvn clean package -DskipTests
-docker build -t hakktastic/demo-product-service:0.0.1 .
-docker push hakktastic/demo-product-service:0.0.1
-cd ..
-
-echo "----------------------- Building Report Service ------------------------"
-cd product
-mvn clean package -DskipTests
-docker build -t hakktastic/demo-report-service:0.0.1 .
-docker push hakktastic/demo-report-service:0.0.1
-cd ..
 
 echo "------------------------------------------------------------------------"
 echo " Deploying Container images on Kubernetes"
