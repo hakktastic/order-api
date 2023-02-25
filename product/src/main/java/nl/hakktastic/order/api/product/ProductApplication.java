e7852d65c68e28f42597fd603aeb6e2fae2adefc
hakktastic <hakktastic@gmail.com>
2023-02-25 15:54:42 +0100

    HAK-107 document openapi of rest service
    to provide documentation according to openapi standards

--- product/src/main/java/nl/hakktastic/order/api/product/ProductApplication.java
+++ product/src/main/java/nl/hakktastic/order/api/product/ProductApplication.java
@@ -1,9 +1,18 @@
 package nl.hakktastic.order.api.product;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Product Service",
+				description = "Demo Rest Product Service for Spring Boot",
+				version = "0.0.1",
+				contact = @Contact(url = "https://www.github.com/hakktastic", email = "hakktastic@gmail.com")))
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProductApplication {
