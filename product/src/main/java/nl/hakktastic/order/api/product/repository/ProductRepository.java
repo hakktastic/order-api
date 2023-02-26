48858bc09389882abcac283d63bfc806ddd9eea6
hakktastic <hakktastic@gmail.com>
2023-02-26 11:51:28 +0100

    HAK-107 add repository stereotype
    to mark class as a repository

--- product/src/main/java/nl/hakktastic/order/api/product/repository/ProductRepository.java
+++ product/src/main/java/nl/hakktastic/order/api/product/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package nl.hakktastic.order.api.product.repository;
 
 import nl.hakktastic.order.api.product.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * JPA repository for {@link ProductEntity}.
  *
  */
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
 }
