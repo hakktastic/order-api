d0120c58533d7c0349479f9a35f5704d5a2066c2
hakktastic <hakktastic@gmail.com>
2023-02-26 09:45:04 +0100

    HAK-107 add read product by id
    to find a product by id

--- product/src/main/java/nl/hakktastic/order/api/product/controller/ProductController.java
+++ product/src/main/java/nl/hakktastic/order/api/product/controller/ProductController.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -20,7 +22,7 @@ import java.util.List;
  *
  */
 @Slf4j
-@RequestMapping("/api")
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 public class ProductController {
 
@@ -38,14 +40,27 @@ public class ProductController {
 
 
     @Operation(summary = "read all products")
-    @ApiResponse(responseCode = "200", description = "orders found in repository")
-    @GetMapping(value = "/products", produces= MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponse(responseCode = "200", description = "product found in repository")
+    @GetMapping(value = "/products")
     public ResponseEntity<List<ProductEntity>> readProducts() {
 
         log.debug("Product Controller - read all products");
 
         return new ResponseEntity<>(service.readProducts()
                 .orElseThrow(() -> new ProductNotFoundException("No product(s) found")),
+                HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "read product with provided ID")
+    @ApiResponse(responseCode = "200", description = "product found in repository")
+    @GetMapping(value = "/products/{id}")
+    public ResponseEntity<ProductEntity> readProduct(@PathVariable @Valid int id) {
+
+        log.debug("Product Controller - read product: productId={}", id);
+
+        return new ResponseEntity<>(service.readProduct(id)
+                .orElseThrow(() -> new ProductNotFoundException("No product found with id="+id)),
                 HttpStatus.OK);
     }
 }
