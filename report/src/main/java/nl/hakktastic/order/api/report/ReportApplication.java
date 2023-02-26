53fb25c1ce5944adcad202971fb23a53c74ac703
hakktastic <hakktastic@gmail.com>
2023-02-26 11:02:48 +0100

    HAK-107 remove @EnableDiscoveryClient
    obsolete

--- report/src/main/java/nl/hakktastic/order/api/report/ReportApplication.java
+++ report/src/main/java/nl/hakktastic/order/api/report/ReportApplication.java
@@ -2,9 +2,7 @@ package nl.hakktastic.order.api.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ReportApplication {
 
