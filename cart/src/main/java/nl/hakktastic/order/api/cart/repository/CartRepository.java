ac62cfc8f4679d227ed1c34412e1dc5c1b538ac7
hakktastic <hakktastic@gmail.com>
2023-02-27 06:57:28 +0100

    HAK-107 document class
    to provide javadoc for class

--- cart/src/main/java/nl/hakktastic/order/api/cart/repository/CartRepository.java
+++ cart/src/main/java/nl/hakktastic/order/api/cart/repository/CartRepository.java
@@ -4,6 +4,9 @@ import nl.hakktastic.order.api.cart.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Cart Repository.
+ */
 @Repository
 public interface CartRepository extends JpaRepository<CartEntity, Integer> {
 }
