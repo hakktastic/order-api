0bb2b1dadd70d8565a21e534261e6cc3905dde85
hakktastic <hakktastic@gmail.com>
2023-02-27 19:49:34 +0100

    HAK-107 enable feign clients
    to discover services on k8s cluster

--- report/src/main/java/nl/hakktastic/order/api/report/ReportApplication.java
+++ report/src/main/java/nl/hakktastic/order/api/report/ReportApplication.java
@@ -2,8 +2,10 @@ package nl.hakktastic.order.api.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("nl.hakktastic.order.api")
 public class ReportApplication {
 
 	public static void main(String[] args) {
