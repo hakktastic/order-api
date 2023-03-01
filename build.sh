ebdc3c7f8a82717dcd52194defc893c5a898c9b4
hakktastic <hakktastic@gmail.com>
2023-03-01 21:44:50 +0100

    HAK-107 refactor build
    - build from parent pom instead of modules
    - remove -DskipTests to enable testing

--- build.sh
+++ build.sh
@@ -2,34 +2,34 @@ echo "------------------------------------------------------------------------"
 echo " Build Order API"
 echo "------------------------------------------------------------------------"
 
+mvn clean
+mvn package
+
 echo "------------------------------------------------------------------------"
 echo " Building Container images"
 echo "------------------------------------------------------------------------"
 
 echo "----------------------- Building Cart Service --------------------------"
-cd cart
-mvn clean package -DskipTests
-docker build -t hakktastic/demo-cart-service:0.0.1 .
+
+docker build -t hakktastic/demo-cart-service:0.0.1 cart/.
 docker push hakktastic/demo-cart-service:0.0.1
-cd ..
+
 
 echo "----------------------- Building Order Service -------------------------"
-cd order
-mvn clean package -DskipTests
-docker build -t hakktastic/demo-order-service:0.0.1 .
+
+docker build -t hakktastic/demo-order-service:0.0.1 order/.
 docker push hakktastic/demo-order-service:0.0.1
-cd ..
+
 
 echo "----------------------- Building Product Service -----------------------"
-cd product
+
 mvn clean package -DskipTests
-docker build -t hakktastic/demo-product-service:0.0.1 .
+docker build -t hakktastic/demo-product-service:0.0.1 product/.
 docker push hakktastic/demo-product-service:0.0.1
-cd ..
+
 
 echo "----------------------- Building Report Service ------------------------"
-cd report
+
 mvn clean package -DskipTests
-docker build -t hakktastic/demo-report-service:0.0.1 .
+docker build -t hakktastic/demo-report-service:0.0.1 report/.
 docker push hakktastic/demo-report-service:0.0.1
-cd ..
